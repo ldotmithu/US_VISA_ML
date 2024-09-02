33ca900b4f1f30764d023c274af9044c4b94e90b
ldotmithu <ldotmithurshan222@gmail.com>
2024-09-02 12:08:44 +0530

    requrements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="US_VISA_ML",
+    version="0.0.0",
+    author="ldotmithu",
+    author_email="ldotmithurshan222@gmail.com",
+    packages=find_packages()
+)
